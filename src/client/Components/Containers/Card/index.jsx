159d8e626e33588dec25638626a7735ce9f32fbe
trip <trip@desktop.lan>
2020-10-17 12:52:00 -0500

    Fixed client build toolchain

---
+++ src/client/Components/Containers/Card/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Card extends Component {
+    render() {
+        return (
+            <div>
+                <h2>I'm a card</h2>
+            </div>
+        )
+    }
+}
---
+++ src/client/Pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Card from '../../Components/Containers/Card';
+
+export default class index extends Component {
+    render() {
+        return (
+            <div>
+                <Card />
+            </div>
+        )
+    }
+}
