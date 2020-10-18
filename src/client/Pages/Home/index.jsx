98d8f169e6fb28839d179364abdeeb1755b48b35
trip <trip@desktop.lan>
2020-10-18 09:55:09 -0500

    Added custom stun server list

--- src/client/Pages/Home/index.jsx
+++ src/client/Pages/Home/index.jsx
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import WebTorrent from 'webtorrent';
 import Card from '../../Components/Containers/Card';
 import StatsCard from '../../Components/StatsCard';
-import SimplePeer from 'simple-peer';
+// import SimplePeer from 'simple-peer';
+import StunServers from '../../../utils/constants/stun-servers';
 import './home.css';
 
 const torrentId = 'magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent'
@@ -14,7 +15,15 @@ export default class Home extends Component {
         const client = new WebTorrent({
             tracker: {
                 rtcConfig: {
-                  ...SimplePeer.config,
+                    iceServers: [
+                        {
+                            urls: [
+                                // 'stun:stun.l.google.com:19302',
+                                // 'stun:global.stun.twilio.com:3478'
+                                ...StunServers
+                            ]
+                        }
+                    ],
                 }
               }
         });
